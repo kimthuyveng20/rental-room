5cd4dcf2da5242c3d84dbd91ed0b8e82587fc7c7
kimthuyveng <kimthuyveng20@gmail.com>
2026-06-09 12:54:13 +0700

    feat: setup docker and gitlab/github actions deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,20 @@
+// ecosystem.config.js
 module.exports = {
   apps: [
     {
-      name: "RENTAL-ROOM-SYSTEM",
-      script: "cmd.exe",
-      args: '/c "pnpm start"',
-      cwd: "D:/rental-room-system",
+      name: "rental-room-system",
+      script: "node_modules/.bin/next",
+      args: "start -p 3001",
+      cwd: "/home/ubuntu/rental-room-system",
+      env_production: {
+        NODE_ENV: "production",
+      },
+      // Load .env.production automatically
+      env_file: "/home/ubuntu/rental-room-system/.env.production",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
     },
   ],
 };
