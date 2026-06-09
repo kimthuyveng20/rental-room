26ce421bd032381d843446f91d8fbe51b4c82321
kimthuyveng <kimthuyveng20@gmail.com>
2026-06-09 12:06:34 +0700

    feat: setup docker and gitlab/github actions deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "RENTAL-ROOM-SYSTEM",
+      script: "cmd.exe",
+      args: '/c "pnpm start"',
+      cwd: "D:/rental-room-system",
+    },
+  ],
+};
